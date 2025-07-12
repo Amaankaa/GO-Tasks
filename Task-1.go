1ba266a1f98734c450fa1901747910ea5610bad1
Amanuel <amaanumararaa@gmail.com>
2025-07-12 15:24:13 +0300

    Complete task 1

---
+++ Task-1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your full name: ")
+	fullName, _ := reader.ReadString('\n')
+	fullName = strings.TrimSpace(fullName) // removes the trailing newline
+
+	var numberOfSubjects int
+	fmt.Print("Enter the number of subjects you took: ")
+	fmt.Scanln(&numberOfSubjects)
+
+	subjectToGrade := make(map[string]int)
+
+	average := 0
+	for i:=0; i<numberOfSubjects; i++{
+		var subject string
+		fmt.Printf("Enter the name of subject(%v): ", i + 1)
+		fmt.Scanln(&subject)
+		var grade int
+		fmt.Printf("Enter the grade of subject(%v): ", i + 1)
+		fmt.Scanln(&grade)
+
+		for grade < 0 || grade > 100 {
+			fmt.Print("Invalid grade. Please enter a valid grade: ")
+			fmt.Scanln(&grade)
+		}
+
+		subjectToGrade[subject] = grade
+		average += subjectToGrade[subject]
+	}
+
+	fmt.Println(fullName)
+	for key, val := range subjectToGrade {
+		fmt.Printf("Subject: %v | Grade: %v", key, val)
+		fmt.Print("\n")
+	}
+	fmt.Println("Average Grade", average/numberOfSubjects)
+}
